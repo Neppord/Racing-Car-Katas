84116fd20be4de08e256c6dec30e5de01ec63b45
Samuel Ytterbrink <neppord@gmail.com>
2020-06-16 14:34:24 +0200

    Install and run prettier

--- typescript/tests/tire_pressure_monitoring_system/alarm.test.ts
+++ typescript/tests/tire_pressure_monitoring_system/alarm.test.ts
@@ -1,11 +1,10 @@
-import {Alarm} from "../../src/tire_pressure_monitoring_system/alarm";
+import { Alarm } from "../../src/tire_pressure_monitoring_system/alarm";
 
-
-describe('Tyre Pressure Monitoring System', function () {
-    describe('Alarm', function () {
-        it('foo', function () {
-            let alarm = new Alarm()
-            expect(alarm.alarmOn()).toBe(false)
-        })
-    })
-})
+describe("Tyre Pressure Monitoring System", function () {
+  describe("Alarm", function () {
+    it("foo", function () {
+      let alarm = new Alarm();
+      expect(alarm.alarmOn()).toBe(false);
+    });
+  });
+});
